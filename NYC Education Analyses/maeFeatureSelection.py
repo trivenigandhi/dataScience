86bbb61cfaaf7f57d7784591b1b007064160918d
Triveni <trivenigandhi@gmail.com>
2018-08-30 10:44:58 +0800

    fix feature selection and visuals

--- NYC Education Analyses/maeFeatureSelection.py
+++ NYC Education Analyses/maeFeatureSelection.py
@@ -1,30 +1,25 @@
-# This function uses the cross validation technique from sci-kit learn to implement a forward stepwise
-# regression. It requires the inputs to be in array form, with the target and feature names as
-# separate array and list objects respectively. The function returns a pandas data frame listing each
-# features individual r2 score as well as the r2 score using a given feature and all features before
-# it. 
+# This function uses the cross validation technique from sci-kit learn to implement a forward stepwise regression. It requires the inputs to be in array form, with the target and feature names as separate array and list objects respectively. The function returns a pandas data frame listing each features individual mean absolute error (mae) as well as the mae using a given feature and all features before it. 
 
-def r2FeatureSelection(data,target,featureNames):
+def maeFeatureSelection(data,target,featureNames):
     import pandas as pd
     import numpy as np
     from sklearn.linear_model import LinearRegression
     from sklearn.model_selection import cross_val_score
 
-    ## Create the individual feature r2 scores and rank
+    ## Measure the individual mean absolute errors of each feature
     lm = LinearRegression()
     output = pd.DataFrame(data=featureNames,columns= ['features'])
     inScores = [cross_val_score(estimator = lm,
                                 X = data[:,x].reshape(-1, 1),
                                 y = target,
                                 cv=5,
-                                scoring="r2").mean() for x in range(len(featureNames))]
+                                scoring="neg_mean_absolute_error").mean() for x in range(len(featureNames))]
     output['individualScores'] = inScores
     
-    ## sort the scores from highest to lowest
-    #output['delta'] = output.individualScores.apply(lambda x: 1- x)
-    output.sort_values(by=['individualScores'],inplace = True)
-    output.drop("delta",axis = 1,inplace = True)
-    #featureIndex = output.index
+    ## sort the errors from highest to lowest
+    output.sort_values(by=['individualScores'],inplace = True, ascending = False)
+    output.reset_index(drop = True, inplace = True)
+    featureIndex = output.index
     
     ## Create output list of features starting with best ranked from earlier,
     ## then successively add each next best feature 
@@ -36,7 +31,7 @@ def r2FeatureSelection(data,target,featureNames):
                                 X = data[:,f],
                                 y = target,
                                 cv=5,
-                                scoring="r2").mean())
+                                scoring="neg_mean_absolute_error").mean())
     output['addedScores'] = addScores
     
     ## return feature selection data 
